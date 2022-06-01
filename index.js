84b574fbebbe1849e40b778f14f018d102501e5e
jerry <jerry_cj_liu@wistron.com>
2022-06-01 15:30:34 +0800

    16-1: Enable security https

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 const helmet = require("helmet");
 const http = require('http');
+const https = require('https');
+const path = require('path');
+const fs = require('fs');
 const server = http.createServer(app)
 const { Server } = require("socket.io");
 const io = new Server(server);
 // Database connect
 const db = require('./config/database')
 const swaggerUI = require("swagger-ui-express")
-const swaggerJsDoc = require("swagger-jsdoc")
+const swaggerJsDoc = require("swagger-jsdoc");
+const PORT= process.env.PORT || 3000;
+const httpsOptions = {
+    key:fs.readFileSync(path.resolve('./certificates/key.pem'), 'utf8'),
+    cert:fs.readFileSync(path.resolve('./certificates/cert.pem'), 'utf8')
+}
 // Test Database
 db.authenticate().then(()=>console.log("DB connect")).catch(err => console.log(err))
 
@@ -46,5 +54,6 @@ app.use((req,res,next)=>{
 // Users routers
 app.use('/users', require('./routers/Users'));
 app.use('/login', require('./routers/Login'));
-const PORT= process.env.PORT || 3000;
+const sslServer = https.createServer(httpsOptions, app)
+sslServer.listen(3443, ()=> console.log("Secure service on port 3443"))
 server.listen(PORT, console.log(`Server started on port ${PORT}`))
